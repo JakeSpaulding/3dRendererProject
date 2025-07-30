633b1895da06c8aa39595f10f1cfb6b9fe2cd6c3
Jake Spaulding <jakemspaulding@gmail.com>
2025-07-30 15:30:29 -0400

    Changed over from vertex structs to a buffer system and finished obj loading

--- TriCastLib.hpp
+++ TriCastLib.hpp
@@ -1,6 +1,20 @@
 #pragma once
 // contains functions for geometry math
 #include "geometry.hpp"
+// applies a matrix transform to all points in a vbo and outputs it to vboout
+inline void projectVBO(std::vector<vec3>& VBOout, std::vector<vec3> const& VBOin, mat4 projMat, int w, int h) {
+	VBOout.clear(); // clear the output
+	VBOout.resize(VBOin.size()); // reserve the propper size
+
+	for (int i = 0; i < VBOin.size(); i++) {
+		VBOout[i] = projMat * VBOin[i]; // project it
+
+		// w = screen width, h = screen height
+		VBOout[i][0] = ((VBOout[i][0] + 1.0f) * 0.5f) * w; // convert to pixel centered coordinates
+		VBOout[i][1] = ((VBOout[i][1] + 1.0f) * 0.5f) * h;
+		std::cout << "\n" << VBOout[i] << "\n";
+	}
+}
 
 // takes 2 vertts and a point, returns 0 if the point is on the line, > 0 of it is right, and <0 if it is left (it's the 2d cross product)
 inline float edgeFunction(vec3 const& A, vec3 const& B, vec3 const& C) {
